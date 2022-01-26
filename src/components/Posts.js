22f3603309b2aa1ade9372ca759369c05ee62dbf
Oscar Del Campo <oscdelcampo@gmail.com>
2022-01-26 18:22:08 -0300

    Prueba actualización amplify

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -6,6 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Card } from '@mui/material';
 
 const Posts = ({ posts }) => {
   const renderItem = (post) => {
@@ -24,7 +25,7 @@ const Posts = ({ posts }) => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell align="center">UserId</TableCell>
+            <TableCell align="center">Prueba actualización amplify</TableCell>
             <TableCell align="center">Id</TableCell>
             <TableCell align="center">Title</TableCell>
             <TableCell align="center">Body</TableCell>
